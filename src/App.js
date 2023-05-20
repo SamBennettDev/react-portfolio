d81a6a4c19557488719da86cbe2c24de0d55654d
Sam Bennett <sambennettdev@gmail.com>
2023-05-20 00:29:15 -0600

    Making progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-} from "./components";
+import { About, Contact, Hero, Navbar, Tech, Works } from "./components";
 
 const App = () => {
   return (
@@ -20,10 +11,8 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
         </div>
