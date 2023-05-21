f2a96bae634d914372584e33612fe3b58f5e7f3b
Sam Bennett <sambennettdev@gmail.com>
2023-05-21 00:05:49 -0600

    added typed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,28 @@
 import { styles } from "../styles";
 import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        `<h1 class="${styles.heroHeadText} text-white"}>Hi, I'm <span class='text-[#3591ef]'>Sam</span></h1></br><span class="${styles.heroSubText} text-white-100">Computer Science student at the University of Utah<br>Expected Graduation - Spring 2024</span>`,
+      ],
+      typeSpeed: 10,
+      backSpeed: 100,
+      startDelay: 300,
+      backDelay: 1000,
+      showCursor: false,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -11,19 +32,8 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#3591ef]" />
           <div className="w-1 sm:h-80 h-40 blue-gradient" />
         </div>
-
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#3591ef]">Sam</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Computer Science student at the University of Utah{" "}
-            <br className="sm:block hidden" />
-            Expected Graduation - Spring 2024
-          </p>
-        </div>
+        <div ref={el}></div>
       </div>
-
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
